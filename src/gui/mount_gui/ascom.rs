1d6425b606d42b61adf930469a35a9691b49c287
Filip Szczerek <ga.software@yahoo.com>
2021-06-26 11:01:54 +0200

    Update file header date

--- src/gui/mount_gui/ascom.rs
+++ src/gui/mount_gui/ascom.rs
@@ -1,6 +1,6 @@
 //
 // Vidoxide - Image acquisition for amateur astronomy
-// Copyright (c) 2020-2021 Filip Szczerek <ga.software@yahoo.com>
+// Copyright (c) 2021 Filip Szczerek <ga.software@yahoo.com>
 //
 // This project is licensed under the terms of the MIT license
 // (see the LICENSE file for details).
